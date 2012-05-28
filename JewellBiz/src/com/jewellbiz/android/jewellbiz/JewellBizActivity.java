bf1f0984899b2cc77b88bd50fa505764306d28b8
jhuntdog <jasonmhuntington@gmail.com>
2012-05-28 09:57:05 -0500

    Initial Commit
    Eclipse Project Added

---
+++ JewellBiz/src/com/jewellbiz/android/jewellbiz/JewellBizActivity.java
@@ -0,0 +1,13 @@
+package com.jewellbiz.android.jewellbiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JewellBizActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
