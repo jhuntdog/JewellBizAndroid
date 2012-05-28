5520e61a278c260069ca2a4e12cf90bb68a24579
jhuntdog <jasonmhuntington@gmail.com>
2012-05-28 17:41:23 -0500

    Functionality added

--- JewellBiz/src/com/jewellbiz/android/jewellbiz/ui/ContentFragment.java
+++ JewellBiz/src/com/jewellbiz/android/jewellbiz/ui/ContentFragment.java
@@ -1,7 +1,116 @@
 package com.jewellbiz.android.jewellbiz.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.jewellbiz.android.jewellbiz.R;
 
 public class ContentFragment extends SherlockFragment {
+	
+	public static final String DEBUG_TAG = "ContentFragment";
+	
+	private WebView mWebView = null;
+	private boolean mSoloFragment = false;
+	private int mCurPosition = 0;
+	
+	public ContentFragment() {
+		super();
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		mWebView = (WebView) inflater.inflate(R.layout.webviewer, container, false);
+		WebSettings webSettings = mWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		return mWebView;
+		
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		// Set member variable for whether this fragment is the only one in the activity
+		Fragment listFragment = getFragmentManager().findFragmentById(R.id.list_frag);
+		mSoloFragment = listFragment == null ? true : false;
+		
+		if (mSoloFragment) {
+			ActionBar actionBar = getSherlockActivity().getSupportActionBar();
+			actionBar.setDisplayHomeAsUpEnabled(true);
+			actionBar.setDisplayShowTitleEnabled(false);
+			// Must call in order to get onOptionsItemSelected callback
+			setHasOptionsMenu(true);
+			
+			/*ViewTreeObserver observer = getView().getViewTreeObserver();
+			observer.addOnGlobalLayoutListener(layoutListener);*/
+		}
+		
+	}
+	
+	/*@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		// Always detach ViewTreeObserver listeners when the view tears down
+		getView().getViewTreeObserver().removeGlobalOnLayoutListener(layoutListener);
+	}*/
+	
+	public void updateUrl(String newUrl) {
+		if (mWebView !=null) {
+			WebSettings webSettings = mWebView.getSettings();
+			webSettings.setJavaScriptEnabled(true);
+			mWebView.loadUrl(newUrl);
+			
+		}
+	}
+	
+	
+	// Options Menu
+	// -----------------
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		getSherlockActivity().getSupportMenuInflater().inflate(R.menu.content_menu, menu);
+		
+		//do stuff for sharing 
+		
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+	
+	@Override
+	public void onPrepareOptionsMenu(Menu menu) {
+		
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent intent = new Intent(getSherlockActivity(), MainActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	@Override
+	public void onSaveInstanceState (Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt("listPosition", mCurPosition);
+	}
 
 }
--- JewellBiz/src/com/jewellbiz/android/jewellbiz/ui/JbzListFragment.java
+++ JewellBiz/src/com/jewellbiz/android/jewellbiz/ui/JbzListFragment.java
@@ -190,84 +190,84 @@ public class JbzListFragment extends SherlockListFragment implements LoaderManag
 	
 	
 	// LoaderManager.LoaderCallbacks<Cursor> methods
-		// ------------------------------------------------------------------------
-		@Override
-		public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-			String[] projection = { JbzDatabase.ID, JbzDatabase.COL_TITLE, 
-					JbzDatabase.COL_DATE, JbzDatabase.COL_READ };
+	// ------------------------------------------------------------------------
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		String[] projection = { JbzDatabase.ID, JbzDatabase.COL_TITLE, 
+				JbzDatabase.COL_DATE, JbzDatabase.COL_READ };
 
-			Uri content = JbzProvider.CONTENT_URI;
-			String selection = null;
-			if (JbzSharedPrefs.getOnlyUnreadFlag(getActivity())) {
-				selection = JbzDatabase.COL_READ + "='0'";		
-			}
-			CursorLoader cursorLoader = new CursorLoader(getActivity(), 
-					content, projection, null, null, JbzDatabase.COL_DATE+" desc");
-			
-			return cursorLoader;
-			
+		Uri content = JbzProvider.CONTENT_URI;
+		String selection = null;
+		if (JbzSharedPrefs.getOnlyUnreadFlag(getActivity())) {
+			selection = JbzDatabase.COL_READ + "='0'";		
 		}
+		CursorLoader cursorLoader = new CursorLoader(getActivity(), 
+				content, projection, null, null, JbzDatabase.COL_DATE+" desc");
+		
+		return cursorLoader;
+		
+	}
 
-		@Override
-		public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-			adapter.swapCursor(cursor);		
-		}
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		adapter.swapCursor(cursor);		
+	}
 
-		@Override
-		public void onLoaderReset(Loader<Cursor> loader) {
-			adapter.swapCursor(null);		
-		}
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		adapter.swapCursor(null);		
+	}
 		
 		
 
-		// custom viewbinder
-		// ------------------------------------------------------------------------
-		private class JbzViewBinder implements SimpleCursorAdapter.ViewBinder {
+	// custom viewbinder
+	// ------------------------------------------------------------------------
+	private class JbzViewBinder implements SimpleCursorAdapter.ViewBinder {
 
-			@Override
-			public boolean setViewValue(View view, Cursor cursor, int index) {
-				if (index == cursor.getColumnIndex(JbzDatabase.COL_READ)) {
-					boolean read = cursor.getInt(index) > 0 ? true : false;
-					TextView title = (TextView) view;
-					if (!read) {
-						title.setTypeface(Typeface.DEFAULT_BOLD, 0);
-					} else {
-						title.setTypeface(Typeface.DEFAULT);
-					}
-					return true;
-				} else if (index == cursor.getColumnIndex(JbzDatabase.COL_DATE)) {
-					// get a locale based string for the date
-					DateFormat formatter = android.text.format.DateFormat
-							.getDateFormat(getActivity().getApplicationContext());
-					long date = cursor.getLong(index);
-					Date dateObj = new Date(date * 1000);
-					((TextView) view).setText(formatter.format(dateObj));
-					return true;
+		@Override
+		public boolean setViewValue(View view, Cursor cursor, int index) {
+			if (index == cursor.getColumnIndex(JbzDatabase.COL_READ)) {
+				boolean read = cursor.getInt(index) > 0 ? true : false;
+				TextView title = (TextView) view;
+				if (!read) {
+					title.setTypeface(Typeface.DEFAULT_BOLD, 0);
 				} else {
-					return false;
+					title.setTypeface(Typeface.DEFAULT);
 				}
-			}			
-		}
-		
-		ViewTreeObserver.OnGlobalLayoutListener layoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
-			
-			@Override
-			public void onGlobalLayout() {
-				int barHeight = getActivity().getActionBar().getHeight();
-				ListView listView = getListView();
-				FrameLayout.LayoutParams params = (LayoutParams) listView.getLayoutParams();
-				// The list view top-margin should always match the action bar height
-				if (params.topMargin != barHeight) {
-					params.topMargin = barHeight;
-					listView.setLayoutParams(params);
-				}
-				// The action bar doesn't update its height when hidden, so make top-margin zero
-				if (!getActivity().getActionBar().isShowing()) {
-					params.topMargin = 0;
-					listView.setLayoutParams(params);
-				}
+				return true;
+			} else if (index == cursor.getColumnIndex(JbzDatabase.COL_DATE)) {
+				// get a locale based string for the date
+				DateFormat formatter = android.text.format.DateFormat
+						.getDateFormat(getActivity().getApplicationContext());
+				long date = cursor.getLong(index);
+				Date dateObj = new Date(date * 1000);
+				((TextView) view).setText(formatter.format(dateObj));
+				return true;
+			} else {
+				return false;
 			}
-		};
+		}			
+	}
+	
+	ViewTreeObserver.OnGlobalLayoutListener layoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
+		
+		@Override
+		public void onGlobalLayout() {
+			int barHeight = getActivity().getActionBar().getHeight();
+			ListView listView = getListView();
+			FrameLayout.LayoutParams params = (LayoutParams) listView.getLayoutParams();
+			// The list view top-margin should always match the action bar height
+			if (params.topMargin != barHeight) {
+				params.topMargin = barHeight;
+				listView.setLayoutParams(params);
+			}
+			// The action bar doesn't update its height when hidden, so make top-margin zero
+			if (!getActivity().getActionBar().isShowing()) {
+				params.topMargin = 0;
+				listView.setLayoutParams(params);
+			}
+		}
+	};
 	
 	
 	
